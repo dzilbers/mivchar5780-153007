73c7f89df9220fbe2e2e400efd7aa24cd1f74f27
Dan Zilberstein <dzilbers@gmail.com>
2019-11-10 21:24:31 +0200

    enumerator

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Inheritance
 {
@@ -25,27 +23,107 @@ namespace Inheritance
         public override void F() { Console.WriteLine("Son.f()"); }
         public override void V() { }
 
-        public T G<T, U>(U parm) { return default(T);  }
+        public T G<T, U>(U parm) { return default(T); }
     }
 
-    class OtherSon : Dad {
+    class OtherSon : Dad
+    {
         public override void F() { Console.WriteLine("OtherSon.f()"); }
     }
 
-    class MyClass //: IComparable<MyClass>
+    class MyClass : IComparable<MyClass>
     {
         int number;
-        public int Number { get { return this.number; } }
-        //public int CompareTo(MyClass other)
-        //{
-        //    return this.number - other.number;
-        //}
+        public int Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+
+        public int CompareTo(MyClass other)
+        {
+            return this.number - other.number;
+        }
 
         public static implicit operator MyClass(int n) { return new MyClass { number = n }; }
+
+        public override string ToString()
+        {
+            return " " + number;
+        }
+    }
+
+    class MyCollection : IEnumerable<MyClass>
+    {
+        MyClass[] objects = new MyClass[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        //public IEnumerator<MyClass> GetEnumerator()
+        //{
+        //    IEnumerable<MyClass> temp = objects;
+        //    return temp.GetEnumerator();
+        //    //return new Enumerator(this);
+        //}
+        public IEnumerator<MyClass> GetEnumerator()
+        {
+            foreach (var elem in objects)
+                yield return elem;
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return objects.GetEnumerator();
+            //return new Enumerator(this);
+        }
+
+        //class Enumerator : IEnumerator<MyClass>
+        //{
+        //    MyCollection coll;
+        //    int current = -1;
+
+        //    public Enumerator(MyCollection myCollection)
+        //    {
+        //        this.coll = myCollection;
+        //    }
+
+        //    object IEnumerator.Current
+        //    {
+        //        get { return coll.objects[current]; }
+        //    }
+
+        //    MyClass IEnumerator<MyClass>.Current
+        //    {
+        //        get { return coll.objects[current]; }
+        //    }
+
+        //    public void Dispose()
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    public bool MoveNext()
+        //    {
+        //        return ++current < coll.objects.Length;
+        //    }
+
+        //    public void Reset() { }
+        //}
+
     }
 
     class Program
     {
+
+        public static IEnumerable<int> ArithSeq(int a0, int d)
+        {
+            yield return a0;
+            while (true)
+            {
+                a0 += d;
+                yield return a0;
+            }
+        }
+
         static void Main(string[] args)
         {
             Son o1 = new Son(5);
@@ -70,10 +148,45 @@ namespace Inheritance
 
             Array.Sort(myObjects);
             foreach (var o in myObjects)
+            {
                 Console.Write(" " + o.Number);
+            }
+
             Console.WriteLine();
 
-            Console.WriteLine(o1.G<char, int>(5));
+            //Console.WriteLine(o1.G<char, int>(5));
+
+            MyClass o5 = 10;
+            IComparable<MyClass> o6 = o5;
+
+            Console.WriteLine(o6.CompareTo(new MyClass() { Number = 1 }));
+
+            MyCollection coll = new MyCollection();
+
+            Console.WriteLine("###################");
+            IEnumerator iter = coll.GetEnumerator();
+            while (iter.MoveNext())
+            {
+                Console.Write(iter.Current);
+            }
+            Console.WriteLine();
+            Console.WriteLine("###################");
+            foreach (var element in coll)
+            {
+                if (element.Number > 5) break;
+                Console.Write(element.Number);
+            }
+
+            Console.WriteLine();
+
+
+            int counter = 15;
+            foreach (int a in ArithSeq(1, 2))
+            {
+                if (--counter == 0) break;
+                Console.Write(" " + a);
+            }
+            Console.WriteLine();
         }
     }
 }
